949c2b183a0171e4b8f76af7bb2b2595d269f0c5
Anup <anup.gj@gmail.com>
2022-07-12 17:24:09 +0530

    DB script renamed

---
+++ RetailRewardSystem.DB/dbo.Transactions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Transactions] (
+    [Id]             INT    IDENTITY (1, 1) NOT NULL,
+    [Date]           DATE   NOT NULL,
+    [CustomerId]     INT    NOT NULL,
+    [PurchaseAmount] BIGINT NOT NULL,
+    [RewardPoints]   BIGINT NULL
+)
+
